5ec0898cb6bbd9faaa07aab34052b04b7432b047
QUANG_CTY <nnduyquang@gmail.com>
2016-10-12 17:29:28 +0700

    admin

--- gulpfile.js
+++ gulpfile.js
@@ -33,6 +33,20 @@ mix.sass([
     'admin/login/style.scss'
 ], 'public/css/login.css');
 
+mix.sass([
+    'bower_components/bootstrap/dist/css/bootstrap.min.cs',
+    'bower_components/font-awesome/css/font-awesome.min.css',
+    'bower_components/Ionicons/css/ionicons.min.css',
+    'bower_components/iCheck/skins/flat/blue.css',
+    'admin/morris/morris.css',
+    'bower_components/jvectormap/jquery-jvectormap.css',
+    'bower_components/datepicker/css/datepicker.css',
+    'admin/daterangepicker/daterangepicker.css',
+    'admin/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css',
+
+
+],'public/admin_third_party.css');
+
 
 mix.scripts([
     // paths.layerslider + 'js/greensock.js',
@@ -46,9 +60,16 @@ mix.scripts([
     'admin/login/index.js'
 ], 'public/js/login.js');
 
-mix.copy([
-    //'resources/assets/fonts/elegant-icons/**',
+mix.scripts([
+    'bower_components/html5shiv/dist/html5shiv.min.js',
+    'bower_components/respond/desr/respond.min.js',
 
-], 'public/fonts');
+],'public/js/admin_third_party.js');
+
+mix.copy([
+    'resources/assets/sass/admin/_all-skins.min.css',
+    'resources/assets/sass/admin/AdminLTE.min.css',
+
+], 'public/css');
 //mix.copy('resources/assets/fonts/js_composer/**','public/fonts');
 });
