7fbd06613a31661f259aeefe3cb96921ad1a784e
QUANGNND <nnduyquang@gmail.com>
2016-10-17 20:56:36 +0700

    Fix Bug Update Location

--- app/locations.php
+++ app/locations.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class locations extends Model
 {
     protected $table='locations';
+    protected $primaryKey = 'id_location';
     protected $fillable=['id_location','name','created_at','updated_at','id'];
     public function users(){
         return $this->belongsTo('App\User','id');
